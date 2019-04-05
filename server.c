c8a22c43acd87b2114ff3fbe8dd899666cc0e8f3
zombieman104 <daymanblake@gmail.com>
2019-04-04 17:07:43 -0700

    Created server.c. included packages that we possibly need.Created a primary socket variable.

---
+++ server.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+//#include <client.c>
+//Not sure which packages to include but for now I'll just include these and delete the uneeded ones in the future. 
+
+//TODO: set up any global variables if needed 
+
+int main() {
+	struct sockaddr_in serv; //Primary socket variable
+
+
+
+}
