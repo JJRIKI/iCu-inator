d25dcffc9e963a98181c66fd207602317952d6a0
James Bek <jjriki40@gmail.com>
2019-04-16 16:21:37 -0700

    Added "hider", our final version of the server Dempsey is going to add the response.

---
+++ hider.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/unistd.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/signal.h>
+
+#define port 28900
+
+#define CMD "netsh wlan show interfaces  | grep BSSID | awk '{print $3}' | tr -d ':\r'"
+//#define pilot "https://pilot.westmont.edu:28900?i=jbek&uptime=60"
+//GET "/?who are you HTTP/1.0\r\\n\r\n"
+
+char * getAP();
+void timer();
+
+
+int main(int argc, char * argv[]) {
+
+	char * AP = getAP();
+	printf(AP);
+
+	pthread_t report;
+	pthread_create(&report, NULL, timer, 0);
+	pthread_join(report, NULL);
+	//getAP(argv[1]) called first
+	//threads: timer & respond(if ANYTHING CONNECTS ==> respond)
+	return 0;
+}
+
+
+char * getAP() {
+	FILE * mycmd = popen(CMD, "r");	
+	char buf[250];
+
+	fgets(buf, sizeof(buf), mycmd);
+	pclose(mycmd);
+
+	char foo[250] = "awk '/";
+	strcat(foo, buf);
+	strtok(foo, "\n");
+	strcat(foo, "/{print NR, $1}' bssids.txt");
+
+	FILE * mycmd2 = popen(foo, "r");
+	char buf2[250];
+
+	fgets(buf2, sizeof(buf2), mycmd2);
+	pclose(mycmd2);
+
+	    int i = 0;
+    char *p = strtok (buf2, " ");
+    char *array[3];
+    while (p != NULL) {
+        array[i++] = p;
+        p = strtok (NULL, " ");
+    }
+
+    int j = 0;
+    char *q = strtok (&array[1], ".");
+    char *array2[3];
+    while (q != NULL) {
+        array2[j++] = q;
+        q = strtok (NULL, ".");
+    }
+
+    int k = 0;
+    char *r = strtok (&array2[1], "-");
+    char *array3[3];
+    while (r != NULL) {
+    	array3[k++] = r;
+    	r = strtok (NULL, "-");
+    }
+
+    char name[100] = "Dempsey ";
+    char array4[10];
+    strtok(array4, "\n");
+    strcat(array4, &array3[1]);
+    strtok(array4, "\n");
+    strcat(array4, "-");
+	strtok(array4, "\n");    
+    strcat(array4, &array3[2]);
+    printf("%s\n", array4);
+
+    strcat(name, array4);
+    printf("%s\n", name);  
+
+    return name;  
+}
+
+
+void timer() {
+	char web[100] = "GET /?i=jbek&uptime=60 HTTP/1.0\r\n\r\n";
+
+	int i = 0;
+	do {
+
+		int sockpilot, ret;
+		struct sockaddr_in serverpilot;
+
+		sockpilot = socket(AF_INET, SOCK_STREAM, 0);
+
+		inet_pton(AF_INET, (char *) "10.115.20.250", &serverpilot.sin_addr);
+		serverpilot.sin_family = AF_INET;
+		serverpilot.sin_port = htons(port);
+
+		ret = connect(sockpilot, (const struct sockaddr *)&serverpilot, sizeof(serverpilot));
+		i++;
+
+		write(sockpilot, web, strlen(web));
+
+		sleep(60);
+
+	} while(1);
+
+}
